6c678d1f638d243b590ca5df4e138fe69b2ec080
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-03 13:43:43 -0400

    Add currency_code file.

---
+++ currency_code.rb
@@ -0,0 +1,11 @@
+require './currency'
+require './currency_converter'
+
+dollar = Currency.new(75, "USD")
+dollar2 = Currency.new(75, "USD")
+
+#print dollar.amount
+#print " "
+#puts dollar.currency_code
+
+puts dollar == dollar2
