2e5240e0592bd7bc7df5a2461ba05b717fffea16
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-03 13:43:24 -0400

    Add equals logic for two currencies with same amount and currency.

--- currency.rb
+++ currency.rb
@@ -8,5 +8,22 @@ class Currency
     @currency_code = currency_code
   end
 
-  
+  def ==(currency_one)
+    @amount == currency_one.amount && @currency_code == currency_one.currency_code
+  end
+
+
+
+
+
+#  def +(amount, amount)
+#  end
+
+
 end
+
+
+#Should equal another Currency object with
+#the same amount and currency code
+#Should NOT equal another Currency object
+#with different amount or currency code
