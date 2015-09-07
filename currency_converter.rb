bdd4b4ffb4905c66905531edbc70e794f433864a
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-06 21:57:09 -0400

    Add clearer logic for convert method, asking if hash includes key, otherwise error.

--- currency_converter.rb
+++ currency_converter.rb
@@ -6,24 +6,21 @@ class CurrencyConverter
     @currency_hash = currency_hash
   end
 
+# def convert (currency, currency_code)
+#    if currency_code != nil
+#      currency.amount * (@currency_hash[currency_code]/@currency_hash[currency.currency_code])
+#    else
+#        raise UnknownCurrencyCodeError
+#    end
+#  end
+
   def convert (currency, currency_code)
-    #if currency.currency_code == currency_code
-    #  currency.amount
-    #elsif currency.currency_code != currency_code
-    if currency.currency_code
-      currency.amount * (@currency_hash[currency_code]/@currency_hash[currency.currency_code])
-    else
-    #TO DO: see if order should be modified here so first unknown currency code error is thrown
-    raise UnknownCurrencyCodeError
-    end
-  end
+     if @currency_hash.has_key?(currency_code)
+       currency.amount * (@currency_hash[currency_code]/@currency_hash[currency.currency_code])
+     else
+         raise UnknownCurrencyCodeError
+     end
+   end
+
 
 end
-
-
-# 1 USD = .84 EUR
-
-# 20 USD = .84(20) EUR
-
-# .84 EUR = 1 USD
-# 20 EUR = 20/.84 USD
