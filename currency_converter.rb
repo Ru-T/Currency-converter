ada9f896cf33cbf1665bf303d7367b8062df897e
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-06 20:58:36 -0400

    Simplify if/else statement for convert method.

--- currency_converter.rb
+++ currency_converter.rb
@@ -7,14 +7,12 @@ class CurrencyConverter
   end
 
   def convert (currency, currency_code)
-    if currency.currency_code == currency_code
-      currency.amount
-    #TO DO: comment out everything but the if if this breaks!
-    #TO DO: check if the above if statement is unnecessary
-    elsif currency.currency_code != currency_code
+    #if currency.currency_code == currency_code
+    #  currency.amount
+    #elsif currency.currency_code != currency_code
+    if currency.currency_code
       currency.amount * (@currency_hash[currency_code]/@currency_hash[currency.currency_code])
     else
-    # put custom error in here
     #TO DO: see if order should be modified here so first unknown currency code error is thrown
     raise UnknownCurrencyCodeError
     end
